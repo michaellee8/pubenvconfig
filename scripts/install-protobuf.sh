e3daa7fc3447af42aa25c31ee6e517f147209b0d
michaellee8 <ckmichael8@gmail.com>
2020-12-16 23:14:32 +0800

    Create install-protobuf.sh

---
+++ scripts/install-protobuf.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# From http://google.github.io/proto-lens/installing-protoc.html
+
+TMPDIR="{${TMPDIR:-$(dirname $(mktemp))/}"
+
+pushd $TMPDIR
+
+PROTOC_ZIP=protoc-3.14.0.-linux-x86_64.zip
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
+sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
+rm -f $PROTOC_ZIP
+
+popd
