b137ab79025748f4a2aedddb4b1cb4bb6c5754ce
michaellee8 <ckmichael8@gmail.com>
2020-12-16 15:24:14 +0000

    fix: proto install script bug

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -2,11 +2,11 @@
 
 # From http://google.github.io/proto-lens/installing-protoc.html
 
-TMPDIR="{${TMPDIR:-$(dirname $(mktemp))/}"
+TMPDIR=$(mktemp -d)
 
 pushd $TMPDIR
 
-PROTOC_ZIP=protoc-3.14.0.-linux-x86_64.zip
+PROTOC_ZIP=protoc-3.14.0-linux-x86_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
