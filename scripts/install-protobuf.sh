07b69b5d8ad30dee89d7820195a6400b5dab993b
michaellee8 <ckmichael8@gmail.com>
2021-05-11 13:09:03 +0000

    fix: update protobuf installer scriptwq

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # From http://google.github.io/proto-lens/installing-protoc.html
 
@@ -6,8 +6,9 @@ TMPDIR=$(mktemp -d)
 
 pushd $TMPDIR
 
-PROTOC_ZIP=protoc-3.14.0-linux-x86_64.zip
-curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
+PROTOC_ZIP=protoc-3.16.0-linux-x86_64.zip
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.16.0/$PROTOC_ZIP
+sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
 sudo chmod -R 0755 /usr/local/bin/protoc /usr/local/include
 rm -f $PROTOC_ZIP
