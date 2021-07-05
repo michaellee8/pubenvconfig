9db2c9ceec5feaf7eabbdadefa2a7cd35c4d0ad4
michaellee8 <ckmichael8@gmail.com>
2021-07-05 21:18:03 +0800

    feat: neovim 0.5.0 config (#1)
    * feat: neovim v0.5.0 config
    
    * feat: wip on nvim5
    
    * feat: wip
    
    * feat: wip
    
    * feat: write neovim config
    
    * feat: wip nvim5 config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,363 @@
+-- michaellee8's init.lua
+-- This file is public domain just in case you want to use it.
+
+-- Follows style guide from lua rocks
+-- https://github.com/luarocks/lua-style-guide
+
+-- helpers
+
+-- From https://github.com/nanotee/nvim-lua-guide.
+local function t(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+-- Auto install packer.nvim.
+
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
+  execute "packadd packer.nvim"
+end
+
+-- Disable flutter tools if a specific environment variable is specified.
+-- Intended for developing Dart SDK or Flutter SDK in-tree.
+
+local no_flutter = vim.env.VIM_NO_FLUTTER == "1" or vim.env.VIM_NO_FLUTTER == "true"
+
+-- Source a specific vimconf.lua in the cwd.
+
+local use_custom_config = vim.env.VIM_USE_CUSTOM_CONFIG == "true" or vim.env.VIM_USE_CUSTOM_CONFIG == "1"
+
+-- Declare packer.nvim plugins.
+
+
+require("packer").startup(function(use)
+
+  -- load packer
+  use "wbthomason/packer.nvim"
+
+  -- treesitter
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+
+  -- native lsp
+  
+  use {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("lspconfig").pyright.setup{
+        on_attach = on_attach,
+      }
+      require("lspconfig").pyright.setup{
+        on_attach = on_attach,
+      }
+
+      require("lspconfig").gopls.setup{
+        on_attach = on_attach,
+      }
+
+      require("lspconfig").jsonls.setup {
+          commands = {
+            Format = {
+              function()
+                vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
+              end
+            }
+          },
+          on_attach = on_attach,
+      }
+      require("lspconfig").rust_analyzer.setup{
+        on_attach = on_attach,
+      }
+
+      require("lspconfig").yamlls.setup{
+        on_attach = on_attach,
+      }
+
+      require("lspconfig").tsserver.setup{
+        on_attach = on_attach,
+      }
+    end
+  }
+
+  use {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      vim.g.symbols_outline = {
+        highlight_hovered_item = true,
+        show_guides = true,
+        auto_preview = true,
+        position = 'right',
+        show_numbers = false,
+        show_relative_numbers = false,
+        show_symbol_details = true,
+        keymaps = {
+            close = "<Esc>",
+            goto_location = "<Cr>",
+            focus_location = "o",
+            hover_symbol = "<C-space>",
+            rename_symbol = "r",
+            code_actions = "a",
+        },
+        lsp_blacklist = {},
+      }
+    end
+  }
+  
+  -- autocomplete
+  
+  use "Raimondi/delimitMate"
+
+  use {
+    "hrsh7th/nvim-compe",
+    config = function()
+      require("compe").setup{
+        enabled = true,
+        autocomplete = true,
+        debug = true,
+        min_length = 1,
+        preselect = 'enable',
+        throttle_time = 80,
+        source_timeout = 200,
+        resolve_timeout = 800,
+        incomplete_delay = 400,
+        max_abbr_width = 100,
+        max_kind_width = 100,
+        max_menu_width = 100,
+        documentation = {
+          border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+          winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+          max_width = 120,
+          min_width = 60,
+          max_height = math.floor(vim.o.lines * 0.3),
+          min_height = 1,
+        },
+
+        source = {
+          path = true,
+          buffer = true,
+          calc = true,
+          nvim_lsp = true,
+          nvim_lua = true,
+          vsnip = true,
+          ultisnips = true,
+          luasnip = true,
+        },
+      }
+    end
+  }
+
+  -- dap support
+
+  use "mfussenegger/nvim-dap"
+
+  use "theHamsta/nvim-dap-virtual-text"
+  
+  use { 
+    "rcarriga/nvim-dap-ui", 
+    requires = {
+      "mfussenegger/nvim-dap"
+    },
+    config = function()
+      require("dapui").setup()
+    end
+  }
+
+  -- telescope
+  
+  use {
+    "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup {}
+    end
+  }
+
+  use {
+    "nvim-telescope/telescope.nvim",
+    requires = {
+      {"nvim-lua/popup.nvim"}, 
+      {"nvim-lua/plenary.nvim"},
+      {"kyazdani42/nvim-web-devicons"},
+    },
+    config = function()
+      require("telescope").setup {}
+    end
+  }
+  -- Optionally depends on ripgrep and fd-find, 
+  -- install by sudo apt install ripgrep fd-find.
+
+
+  -- other plugins
+
+  use {
+    "lewis6991/gitsigns.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      require("gitsigns").setup()
+    end
+  }
+
+  use "tpope/vim-fugitive"
+
+  use "tpope/vim-eunuch"
+
+  use "tpope/vim-surround"
+
+  use "tpope/vim-commentary"
+  
+  use "editorconfig/editorconfig-vim"
+  
+  use "christoomey/vim-tmux-navigator"
+
+  use "Yggdroot/indentLine"
+
+  -- Will this break flutter-tools.nvim?
+  -- use "dart-lang/dart-vim-plugin"
+
+  use "vim-airline/vim-airline"
+
+  use "vim-airline/vim-airline-themes"
+
+  use "tpope/vim-obsession"
+
+  use {
+    "prettier/vim-prettier",
+    run = "yarn install",
+  }
+
+  use "junegunn/fzf"
+
+  use {
+    "junegunn/fzf.vim",
+    requires = {
+      "junegunn/fzf",
+    },
+  }
+
+  -- flutter specific
+
+  if not no_flutter then
+    use {
+      "akinsho/flutter-tools.nvim",
+      requires = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+      },
+      config = function()
+        require("flutter-tools").setup{
+          debugger = {
+            enabled = true,
+          },
+          flutter_path = "/home/michaellee8/.local/lib/flutter/bin/flutter",
+          widget_guides = {
+            enabled = true,
+          },
+        }
+        require("telescope").load_extension("flutter")
+      end
+    }
+  end
+  
+end)
+
+-- lsp config
+
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+end
+
+-- autocomplete config
+
+vim.o.completeopt = "menuone,noselect"
+
+
+-- dap related config
+
+vim.g.dap_virtual_text = true
+
+
+
+
+-- keymap
+
+-- nvim-compe suggested keymap
+
+local function set_nvim_compe_keymap(l, r)
+  vim.api.nvim_set_keymap("n", l, r, {
+    noremap = true, 
+    silent = true, 
+    expr = true, 
+  })
+end
+
+set_nvim_compe_keymap("<C-Space>", "compe#complete()")
+-- Not sure if this is the corrrect way for lua,
+-- see https://github.com/hrsh7th/nvim-compe/commit/a7ea48b856841518cd9f542338a69f5165a65de4
+-- set_nvim_compe_keymap("<CR>", 'compe#confirm({ "keys": "\<Plug>delimitMateCR", "mode": "" }')
+vim.cmd([[
+inoremap <silent><expr> <CR> compe#confirm({ 'keys': "\<Plug>delimitMateCR", 'mode': '' })
+]])
+set_nvim_compe_keymap("<C-e>", "compe#close('<C-e>')")
+set_nvim_compe_keymap("<C-f>", "compe#scroll({ 'delta': +4 })")
+set_nvim_compe_keymap("<C-d>", "compe#scroll({ 'delta': -4 })")
+
+-- symbols-outline.nvim
+
+vim.api.nvim_set_keymap("n", "<F8>", ":SymbolsOutline<CR>", { 
+  silent = true 
+})
+
+-- core settings
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.filetype= "on"
+
+vim.o.colorcolumn = "80"
+
+-- 2 spaces for indention
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smarttab = true
+vim.o.mouse = "a"
+
+vim.o.statusline = vim.o.statusline .. "%F"
+
